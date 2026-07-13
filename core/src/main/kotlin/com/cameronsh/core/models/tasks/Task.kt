896db6aef70507d74bd4616f18e2b2d6938e88ac
Cameron Hiscock <cameronsh9421@gmail.com>
2026-07-12 21:17:04 -0700

    Create inital DataFactory.

--- core/src/main/kotlin/com/cameronsh/core/models/tasks/Task.kt
+++ core/src/main/kotlin/com/cameronsh/core/models/tasks/Task.kt
@@ -2,6 +2,8 @@ package com.cameronsh.core.models.tasks
 
 import com.cameronsh.utils.Id
 
+import com.cameronsh.core.models.data.Data
+
 data class Task(
     val name: String,
     var state: TaskState = TaskState.PENDING,
@@ -9,4 +11,5 @@ data class Task(
     val action: () -> Unit
 ) {
     val id: String = Id.genId()
+    val data: Data
 }
