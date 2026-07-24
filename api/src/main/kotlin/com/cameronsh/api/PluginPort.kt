3e483100c89bdc89132771f5ad427913cd93570b
Cameron Hiscock <cameronsh9421@gmail.com>
2026-07-24 01:28:38 -0700

    Initial Rust pipeline framework started.

---
+++ api/src/main/kotlin/com/cameronsh/api/PluginPort.kt
@@ -0,0 +1,8 @@
+package com.cameronsh.api
+
+import com.cameronsh.utils.Id
+import java.util.UUID
+
+class PluginPort() {
+    public val id: UUID = Id.genId()
+}
--- systems/src/pipeline/pipeline.rs
+++ systems/src/pipeline/pipeline.rs
@@ -6,12 +6,26 @@ pub use crate::linker;
 pub use crate::messenger;
 pub use crate::auth;
 
-pub struct pipeline{
-    origin: &str,
-    destination: &str,
+use uuid::Uuid;
+
+pub struct Pipeline{
+    origin: Uuid,
+    destination: Uuid,
     max_messages: i32,
 }
 
 let pipelines: Vec<pipeline> = Vec::new();
 
-fn create_pipeline()
+fn create_pipeline(
+    origin: Uuid,
+    destination: Uuid,
+    max_messages: i32,
+) -> Pipeline {
+    let pipeline = Pipeline{
+        origin: origin,
+        destination: destination,
+        max_messages: max_messages,
+    };
+    pipelines.push(pipeline);
+    return pipeline
+}
