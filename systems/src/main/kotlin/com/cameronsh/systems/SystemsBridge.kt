91a9511b10a9f325b111e6db2ab2cefadcecdea8
Cameron Hiscock <cameronsh9421@gmail.com>
2026-08-14 15:17:30 -0700

    Fill out create IOStream function in Kotlin and add converse functions for UUID and TARGETS_LAYOUT.

--- systems/src/main/kotlin/com/cameronsh/systems/SystemsBridge.kt
+++ systems/src/main/kotlin/com/cameronsh/systems/SystemsBridge.kt
@@ -55,9 +55,52 @@ object SystemsBridge {
         "free_str", FunctionDescriptor.ofVoid(ValueLayout.ADDRESS)
     )
 
-    /*
+    private val TARGETS_LAYOUT = MemoryLayout.structLayout(
+        MemoryLayout.sequenceLayout(2, MemoryLayout.sequenceLayout(16, ValueLayout.JAVA_BYTE)).withName("values")
+    ).withName("Targets")
+
     private val createIostreamHandle = handle(
-        "create_iostream", FunctionDescriptor
+        "create_iostream", FunctionDescriptor.of(
+            ValueLayout.ADDRESS,
+            ValueLayout.ADDRESS,
+            ValueLayout.ADDRESS,
+            TARGETS_LAYOUT,
+        )
     )
-    */
+
+    fun UUIDtoTargets(segment: MemorySegment, idx: Int, id: UUID) {
+        val slice: MemorySegment = segment.asSlice(
+            TARGETS_LAYOUT.byteOffset(
+                MemoryLayout.PathElement.groupElement("values"),
+                MemoryLayout.PathElement.sequenceElement(idx.toLong()),
+            ),
+            16,
+        )
+        slice.set(ValueLayout.JAVA_LONG_UNALIGNED, 0, id.mostSignificantBits)
+        slice.set(ValueLayout.JAVA_LONG_UNALIGNED, 8, id.leastSignificantBits)
+    }
+
+    fun TargetstoUUID(segment: MemorySegment, idx: Int): UUID {
+        val slice: MemorySegment = segment.asSlice(
+            TARGETS_LAYOUT.byteOffset(
+                MemoryLayout.PathElement.groupElement("values"),
+                MemoryLayout.PathElement.sequenceElement(idx.toLong()),
+            ),
+            16,
+        )
+        val msb = slice.get(ValueLayout.JAVA_LONG_UNALIGNED, 0)
+        val lsb = slice.get(ValueLayout.JAVA_LONG_UNALIGNED, 8)
+        return UUID(msb, lsb)
+    }
+
+    fun create_iostream(ids: Array<UUID>) {
+        val env: Any? = null
+        val obj: Any? = null
+        val targets: MemorySegment = arena.allocate(TARGETS_LAYOUT)
+        UUIDtoTargets(targets, 0, ids[0])
+        UUIDtoTargets(targets, 1, ids[1])
+        Arena.ofShared().use { callArena ->
+            createIostreamHandle.invoke(env, obj)
+        }
+    }
 }
