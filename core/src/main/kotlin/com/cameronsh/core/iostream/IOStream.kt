1c8061658b8f02aa4a382aea3d07551e05d1b603
Cameron Hiscock <cameronsh9421@gmail.com>
2026-09-01 12:11:27 -0700

    Fill out IOStream initialization.

--- core/src/main/kotlin/com/cameronsh/core/iostream/IOStream.kt
+++ core/src/main/kotlin/com/cameronsh/core/iostream/IOStream.kt
@@ -4,6 +4,7 @@ import com.cameronsh.utils.Id
 import java.util.UUID
 
 import com.cameronsh.core.ProcessWorker
+import com.cameronsh.core.iostream.pipeline.Pipeline
 import com.cameronsh.core.iostream.port.Port
 
 class IOStream(
@@ -25,8 +26,29 @@ class IOStream(
             println("${name} failed initialization: ${e}")
         }
 
+        var i = 0
         for(target in targets) {
-            
+            val port = Port(
+                name = "${name}Port${i}",
+                host = target,
+            )
+            i++
+        }
+        i = 0
+        while(i < ports.size) {
+            val pipeline0 = Pipeline(
+                name = "${ports[i].name}-${ports[i+1].name}Pipeline",
+                origin = ports[i],
+                destination = ports[i+1],
+            )
+            ports[i].targets.putIfAbsent(ports[i+1].id, pipeline0)
+            val pipeline1 = Pipeline(
+                name = "${ports[i+1].name}-${ports[i].name}Pipeline",
+                origin = ports[i+1],
+                destination = ports[i],
+            )
+            ports[i+1].targets.putIfAbsent(ports[i].id, pipeline1)
+            i += 2
         }
     }
 }
